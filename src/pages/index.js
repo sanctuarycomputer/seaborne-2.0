6dc8bccd2e117e68263cf2d4cd604e38f41c5677
Prabhav Khandelwal <prabhav@alright.studio>
2022-03-28 15:35:58 -0400

    reordering sections and removing old contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,66 +27,6 @@ export default function Home() {
       </section>
 
       <section
-        className="impact-section flex flex-col sm:flex-row justify-between items-center inner-content-max-width site-padding-x mxauto fade-in pb6"
-      >
-        <div className="flex flex-col sm:col-4 pb3 sm:pb0">
-          <h2 className="text-subheader color-black pb2 sm:pb1_75 md:pb2_5">
-            We'll help you understand your impact.
-          </h2>
-          <p className="text-body color-gray pb2 sm:pb1_75 md:pb2_5">
-            We are a team of strategists, designers, and technologists who help founders run more
-            sustainable businesses. We want your business to do good to the environment.
-          </p>
-          <div className="none md:flex flex-row">
-            <div className="flex items-start">
-              <img className="arrow pr2" src="img/arrow.svg" alt="Blue arrow on a light blue circle" />
-            </div>
-            <p className="text-body-sm color-gray mt1">
-              We are using our product experience to help founders launch responsible products and
-              businesses.
-            </p>
-          </div>
-        </div>
-        <div className="impact-image sm:col-6">
-          <img
-            className="w100"
-            src="img/impact-illustration.svg"
-            alt="Illustration of person holding a flower at the beach"
-          />
-        </div>
-      </section>
-
-      <section
-        className="journey-section flex flex-col-reverse sm:flex-row justify-between items-center inner-content-max-width site-padding-x mxauto fade-in"
-      >
-        <div className="journey-image sm:col-6">
-          <img
-            className="w100"
-            src="img/journey-illustration.svg"
-            alt="Illustration of two people in nature."
-          />
-        </div>
-        <div className="flex flex-col sm:col-4 pb3 sm:pb0">
-          <h2 className="text-subheader color-black pb2 sm:pb1_75 md:pb2_5">
-            Let’s start on your journey.
-          </h2>
-          <p className="text-body color-gray sm:pb1_75 md:pb2_5">
-            We use research-backed methods to create clear and informative insights. We don’t
-            believe jargon or academic writing—we believe in taking action.
-          </p>
-          <div className="none md:flex flex-row">
-            <div className="flex items-start">
-              <img className="arrow pr2" src="img/arrow.svg" alt="Blue arrow on a light blue circle" />
-            </div>
-            <p className="text-body-sm color-gray mt1">
-              We use a different type of communication to our clients to help them understand their
-              impact.
-            </p>
-          </div>
-        </div>
-      </section>
-
-      <section
         className="offerings-section items-center justify-center flex flex-col relative bg-color-pink py6 fade-in"
       >
         <div className="absolute t0 w100">
@@ -160,27 +100,65 @@ export default function Home() {
       </section>
 
       <section
-        className="contact-section flex flex-col sm:flex-row justify-between items-center inner-content-max-width mxauto contact-page sm:pt4 sm:pb5 site-padding-x fade-in"
+        className="impact-section flex flex-col sm:flex-row justify-between items-center inner-content-max-width site-padding-x mxauto fade-in pb6"
       >
-        <div className="flex flex-col col-12 sm:col-5 pb3 pt6 sm:pb0 items-center sm:items-start">
-          <p className="text-body color-gray pb2 sm:pb1_75 md:pb2_5">Ready to use less?</p>
-          <p className="text-subheader color-black pb2 sm:pb1_75 md:pb2_5">hello@seaborne.nyc</p>
-          <a
-            aria-label="Get in touch with Seaborne"
-            className="event-contact-us button--style-primary text-button flex items-center justify-center"
-            href="mailto:hello@seaborne.nyc"
-          >
-            Get in touch
-          </a>
+        <div className="flex flex-col sm:col-4 pb3 sm:pb0">
+          <h2 className="text-subheader color-black pb2 sm:pb1_75 md:pb2_5">
+            We'll help you understand your impact.
+          </h2>
+          <p className="text-body color-gray pb2 sm:pb1_75 md:pb2_5">
+            We are a team of strategists, designers, and technologists who help founders run more
+            sustainable businesses. We want your business to do good to the environment.
+          </p>
+          <div className="none md:flex flex-row">
+            <div className="flex items-start">
+              <img className="arrow pr2" src="img/arrow.svg" alt="Blue arrow on a light blue circle" />
+            </div>
+            <p className="text-body-sm color-gray mt1">
+              We are using our product experience to help founders launch responsible products and
+              businesses.
+            </p>
+          </div>
         </div>
-        <div className="sm:col-6 pt6 sm:pt0">
+        <div className="impact-image sm:col-6">
           <img
             className="w100"
-            src="img/contact-illustration.svg"
-            alt="Illustration of person inside a large microphone yelling into an ear."
+            src="img/impact-illustration.svg"
+            alt="Illustration of person holding a flower at the beach"
           />
         </div>
       </section>
+
+      <section
+        className="journey-section flex flex-col-reverse sm:flex-row justify-between items-center inner-content-max-width site-padding-x mxauto fade-in"
+      >
+        <div className="journey-image sm:col-6">
+          <img
+            className="w100"
+            src="img/journey-illustration.svg"
+            alt="Illustration of two people in nature."
+          />
+        </div>
+        <div className="flex flex-col sm:col-4 pb3 sm:pb0">
+          <h2 className="text-subheader color-black pb2 sm:pb1_75 md:pb2_5">
+            Let’s start on your journey.
+          </h2>
+          <p className="text-body color-gray sm:pb1_75 md:pb2_5">
+            We use research-backed methods to create clear and informative insights. We don’t
+            believe jargon or academic writing—we believe in taking action.
+          </p>
+          <div className="none md:flex flex-row">
+            <div className="flex items-start">
+              <img className="arrow pr2" src="img/arrow.svg" alt="Blue arrow on a light blue circle" />
+            </div>
+            <p className="text-body-sm color-gray mt1">
+              We use a different type of communication to our clients to help them understand their
+              impact.
+            </p>
+          </div>
+        </div>
+      </section>
+
     </main>
   );
 }
