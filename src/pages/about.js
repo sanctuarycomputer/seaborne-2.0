ee9b6551bd45961ac042d5f2ce8e6fbfbd3c062a
Prabhav Khandelwal <prabhav@alright.studio>
2022-04-03 01:00:27 -0400

    adding sample content for about heero

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,21 @@
 export default function About() {
   return (
     <main>
-      
+      <section className="about-hero">
+        <p className="text-body">
+          How we work
+        </p>
+
+        <h1 className="text-header">
+          We work with companies that want to take meaningful action on contemporary social and environmental issues, guiding them through each step of the way from strategy to storytelling. 
+        </h1>
+
+        <img 
+          className="separator-arrow"
+          alt="Section separator"
+          src="img/squiggle-arrow.svg"
+        />
+      </section>
     </main>
   );
 }
