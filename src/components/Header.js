a476beb795ce8caf06eb5b13367c68dde8453c59
Prabhav Khandelwal <prabhavkhandelwal@gmail.com>
2022-04-08 11:56:36 -0400

    header - BEM

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ export default function Header() {
     <header
       className="header z-overlay relative"
     >
-      <div className="desktop-nav-container mxauto flex justify-between items-center flex-row pt1 px1_5 py1_5">
+      <div className="header__nav-container--desktop mxauto flex justify-between items-center flex-row pt1 px1_5 py1_5">
         <a 
           href="/"
           rel="noopener noreferrer"
@@ -30,20 +30,20 @@ export default function Header() {
         </a>
         
         <button 
-          className={`nav-toggle flex flex-col md:none bg-color-transparent ${isMobileNavActive ? "mobile-nav-active" : ""}`}
+          className={`header__nav-toggle flex flex-col md:none bg-color-transparent ${isMobileNavActive ? "mobile-nav-active" : ""}`}
           onClick={handleMobileNavToggle}
         >
           <span className="transition"></span>
           <span className="transition"></span>
         </button>
 
-        <nav className="desktop-nav none md:flex flex-row items-center text-body-sm">
+        <nav className="header__nav--desktop none md:flex flex-row items-center text-body-sm">
           <a href="/about" className="color-gray">How we work</a>
           <a href="/studies" className="color-gray">Case Studies &amp; Blog Posts</a>
           <a
             aria-label="Get in touch with Seaborne"
             href="mailto:hello@seaborne.nyc"
-            className="event-contact-us button--style-blue-border nav-button flex items-center"
+            className="button--style-blue-border nav-button flex items-center"
           >
             Get in touch
           </a>
@@ -51,10 +51,10 @@ export default function Header() {
       </div>
 
       <div 
-        className={`mobile-nav-container absolute pt3 px1_5 bg-color-white w100 transition-longer opacity-0 hidden z-0 ${isMobileNavActive ? "active" : ""}`}
+        className={`header__nav-container--mobile absolute pt3 px1_5 bg-color-white w100 transition-longer opacity-0 hidden z-0 ${isMobileNavActive ? "active" : ""}`}
       >
         <nav 
-          className={`none mobile-nav flex-col text-header-sm transition-longer ${isMobileNavActive ? "active" : ""}`}
+          className={`none header__nav--mobile flex-col text-header-sm transition-longer ${isMobileNavActive ? "active" : ""}`}
         >
           <a href="/about" className="color-gray">How we work</a>
           <a href="/studies" className="color-gray">Case Studies &amp; Blog Posts</a>
