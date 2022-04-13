da2d6e352ac9a3f7cba061ccdf0b3575a9e3c861
Prabhav Khandelwal <prabhav@alright.studio>
2022-04-13 19:59:21 -0400

    WIP article header/metadata

--- src/pages/studies/[slug].js
+++ src/pages/studies/[slug].js
@@ -1,9 +1,56 @@
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 export default function StudyShow(props) {
+  // splitting tags string into an array
+  const tagString = props.frontmatter.tags;
+  const tagArray = tagString.split(',');
+
   return (
-    <ReactMarkdown children={props.markdownBody} />
+    <main className="article">
+      <header className="site-padding-x article__header inner-content-max-width flex flex-col items-center">
+        <figure className="col-12">
+          {props.frontmatter.hero_image &&
+            <Image
+              className="article__header-image w100 mxauto" 
+              src={props.frontmatter.hero_image} 
+              alt={props.frontmatter.hero_image_caption} 
+              layout="responsive"
+              width={1248}
+              height={677}
+              priority="true"
+            />
+          }
+          
+          <figcaption className="article__header-image-caption text-body-sm mb3_5 md:mb3 lg:mb4_5">
+            {props.frontmatter.hero_image_caption} 
+          </figcaption>
+        </figure>
+
+        {props.frontmatter.type &&
+          <p className="article__header-type col-12 text-body color-gray">
+            {props.frontmatter.type}
+          </p>
+        }
+        
+        <h1 className="article__header-title col-12 text-header">
+          {props.frontmatter.title}
+        </h1>
+
+        {props.frontmatter.tags &&
+          <div className="article__header-tags col-12">
+            {tagArray.map((tag) => <span key={tag}>{tag}</span>)}
+          </div>
+        }
+
+      </header>
+
+        
+      <article className='article__body'>
+        <ReactMarkdown children={props.markdownBody} />
+      </article>
+    </main>
   );
 }
 
