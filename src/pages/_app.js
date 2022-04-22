ce405d92daaed7d8b355322f0b9cc03f00ff58e3
Prabhav Khandelwal <prabhavkhandelwal@gmail.com>
2022-04-22 15:55:12 -0400

    crossorigin React version

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -30,7 +30,7 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
         /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,600&display=swap" rel="stylesheet" />
         <title>Seaborne — Your guides to sustainable business practices</title>
         <meta
