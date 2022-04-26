5387ab894dbf0cb13e8234be422381a4dbe58e2b
Prabhav Khandelwal <prabhavkhandelwal@gmail.com>
2022-04-26 14:14:55 -0400

    WIP 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,8 @@ import Link from 'next/link';
 
 export default function Custom404() {
   return (
-    <main className="custom404 py5 site-padding-x flex flex-col items-center inner-content-max-width">
-      <p className="text-eyebrow mb3">
-        Error 404 / Let's get you home.
-      </p>
-
-      <figure className="col-12 md:col-4">
+    <main className="custom404 py3 md:py5 site-padding-x mxauto flex flex-col items-center text-center inner-content-max-width">
+      <figure className="col-12 md:col-4 sm:col-8">
         <Image
           className=""
           src="/img/journey-illustration.svg"
@@ -18,19 +14,23 @@ export default function Custom404() {
           layout="responsive"
         />
       </figure>
+
+      {/* <p className="text-footer color-gray mt1_5 mb1">
+        Error 404 / Let's get you home.
+      </p> */}
       
-      <h1 className="text-subheader-alt color-black mt3 mb_5">
+      <h1 className="text-subheader-alt color-black mt3_5 md:mb_5 mb1">
         Sorry, we couldn&rsquo;t find what you were looking for.
       </h1>
 
-      <p className="text-body-light color-gray mb2_5">
-        Learn about <Link href="/about">how we work</Link> and see <Link href="/studies">how we're making a difference</Link>.
+      <p className="text-body-light color-gray mb3_5">
+        Learn about <Link href="/about"><a className="color-blue">how we work</a></Link> and see how we're <Link href="/studies"><a className="color-blue">making a difference</a></Link>.
       </p>
 
       <Link href="/">
         <a
           aria-label="Homepage"
-          className="button--style-primary text-body-sm flex items-center justify-center font-500"
+          className="button--style-primary text-body-sm font-500"
         >
           Back to homepage
         </a>
