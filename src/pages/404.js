6e50babf00303039c7b9c4b8d167a2a2a8911806
Prabhav Khandelwal <prabhavkhandelwal@gmail.com>
2022-05-02 12:50:52 -0400

    Adding custom 404 page (#8)
    * WIP article header/metadata
    
    * replacing single quotes with &rsquo;
    
    * fixing build errors
    
    Link tags for anchors, &rsquo; for quotes and passing articleBody not as children
    
    * no Image
    
    * deleted package-lock.json
    
    * double quotes classname
    
    * article header WIP
    
    * adding metadata for article page
    
    * deleting placeholder article pages
    
    * linking case study template to index
    
    * added 1 placeholder case study
    
    * cleanup
    
    * cleanup
    
    * tag letter spacing, hero caption top margin
    
    * tag classes cleanup
    
    * <Image> updates on about
    
    * <Image> update for index
    
    * case study card - Image update
    
    updated CSS to match styling
    
    * header <Image> update - tweaked styling
    
    * WIP 404
    
    * WIP 404
    
    * rsquo; typo
    
    * lg:padding for 404
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    Co-authored-by: Prabhav Khandelwal <prabhav@alright.studio>

---
+++ src/pages/404.js
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <main className="custom404 py3 md:py5 lg:py8 site-padding-x mxauto flex flex-col items-center text-center inner-content-max-width">
+      <figure className="col-12 md:col-4 sm:col-8">
+        <Image
+          src="/img/journey-illustration.svg"
+          alt="Person holding a flower at the beach."
+          width={545}
+          height={499}
+          layout="responsive"
+        />
+      </figure>
+      
+      <h1 className="text-subheader-alt color-black mt3_5 md:mb_5 mb1">
+        Sorry, we couldn&rsquo;t find what you were looking for.
+      </h1>
+
+      <p className="text-body-light color-gray mb3_5">
+        Learn about <Link href="/about"><a className="color-blue">how we work</a></Link> and see how we&rsquo;re <Link href="/studies"><a className="color-blue">making a difference</a></Link>.
+      </p>
+
+      <Link href="/">
+        <a
+          aria-label="Homepage"
+          className="button--style-primary text-body-sm font-500"
+        >
+          Back to homepage
+        </a>
+      </Link>
+    </main>
+  );
+}
